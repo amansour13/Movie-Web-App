f7af28f7283dd4a9ea71e9a02564c31e15bad5c5
amansour13 <a.mansour.dev1@gmail.com>
2023-09-16 13:37:34 +0300

    update details page

--- src/pages/DetailedMovie.jsx
+++ src/pages/DetailedMovie.jsx
@@ -1,7 +1,31 @@
-  
+import Navbar from "../components/Navbar";
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 function DetailedMovie() {
+
+    const param = useParams();
+
+    const [res, setRes] = useState({});
+    const [load, setLoad] = useState(false);
+    useEffect(() => {
+        async function fetchData  (){
+            const response = await axios.get(`https://api.tvmaze.com/shows/${param.id}`);
+            setRes(response);
+            setLoad(true);
+            console.log(res);
+        }
+        fetchData();
+    },[]);
 return (
     <>
+        <Navbar />
+
+        {
+            (load && res) ?
+                <div>{res.title}</div>
+            : console.log("error")
+        }
     </>
 )}
 
